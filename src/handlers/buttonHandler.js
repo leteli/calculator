6a7bace50fa6d9e70d9fc197c73d542e6a68b8d0
Nastia Kartseva <danetzachem@gmail.com>
2022-09-12 16:36:41 +0300

    Implement basic button handler

---
+++ src/handlers/buttonHandler.js
@@ -0,0 +1,36 @@
+  const buttonValues = [
+    'Rad', 'x!', '%', '(', ')',
+    'Deg', 'sin', 'ln', 7, 8, 9, '÷',
+    'Inv', 'cos', 'log',  4, 5, 6, '×',
+    'π', 'tan', '√', 1, 2, 3, '-',
+    'e', 'EXP', 'xⁿ', 0, '.', '=', '+',
+  ];
+  const operators = /\+|\-|\÷|\%|\×|\!/g;
+
+const buttonHandler = (e, state) => {
+  const buttonValue = e.target.innerHTML;
+  const output = state.outputExpression;
+  switch(buttonValue) {
+    case 'Rad':
+    case 'Deg':
+      state.angleUnit = value;
+      break;
+    case 'Inv':
+      state.isInverted = !state.isInverted;
+      break;
+    case 'x!':
+      output.push('!');
+      break;
+    case 'EXP':
+      output.push('E');
+      break;
+    case 'xⁿ':
+      output.push('**');
+      break;
+    default:
+      output.push(value);
+      break;
+  }
+};
+
+export default buttonHandler;
