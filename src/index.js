9f2b86b46607e84333dde24fff29c257440544aa
Nastia Kartseva <danetzachem@gmail.com>
2022-09-13 20:40:05 +0300

    update initial function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import render from './view.js';
+import render, { initialRender } from './view.js';
 import buttonHandler from "./handlers/buttonHandler.js";
 import equalsHandler from "./handlers/equalsHandler.js";
 import clearHandler from "./handlers/clearHandler.js";
 
 const app = () => {
+
   const state = {
-    outputExpression: [null],
-    resultValue: '',
+    buttonValues: [
+      'Rad', 'x!', '%', '(', ')', '', 'AC',
+      'Deg', 'sin', 'ln', 7, 8, 9, '÷',
+      'Inv', 'cos', 'log',  4, 5, 6, '×',
+      'π', 'tan', '√', 1, 2, 3, '-',
+      'e', 'EXP', 'xⁿ', 0, '.', '=', '+',
+    ],
+    outputExpression: [],
+    resultValue: null,
     isInverted: false,
     angleUnit: 'Rad',
   }
 
-  const buttons = document.getElementByClassName('buttons-item');
+  initialRender(state);
+
+  const buttons = document.getElementsByClassName('buttons-item');
   Array.from(buttons).forEach((button) => {
     const { classList } = button;
     const currentHandler = classList.contains('equals') ? equalsHandler
@@ -27,5 +37,7 @@ const app = () => {
       }));
   });
 };
+
+app();
 
 export default app;
