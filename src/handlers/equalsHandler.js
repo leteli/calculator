a1b3f7fde235079d2e6a5f09fdb05ed7f3f3c10c
Nastia Kartseva <danetzachem@gmail.com>
2022-09-13 20:41:16 +0300

    Implement new equalsHandler with polish notation

--- src/handlers/equalsHandler.js
+++ src/handlers/equalsHandler.js
@@ -1,64 +1,16 @@
-import getTrigFn, { getFactorial, degToRad } from '../utils.js';
+// import getTrigFn, { getFactorial, degToRad, isOperator } from '../utils.js';
+import buildPolishNotation from '../buildPolishNotation.js';
+import calcInPolishNotation from '../calcInPolishNotation.js';
+// продумать приоритет операторов
 
 const equalsHandler = (e, state) => {
-  let skippedValue;
-
-  const getAngle = (value) => state.angleUnit === 'Rad' ? value : degToRad(value);
-
-  const result = state.outputExpression.reduce((prevValue, value, i) => {
-    if (Array.isArray(value)) {
-      equalsHandler(value);
-    }
-
-    const nextValue = state.outputExpression[i + 1];
-
-    if (typeof value !== 'number' && value !== '!') { // включая PI, E
-      skippedValue = nextValue;
-    }
-
-    switch(value) {
-      case skippedValue:
-        return prevValue;
-      case '+':
-        return prevValue + nextValue;
-      case '-':
-        return prevValue - nextValue;
-      case '×':
-        return prevValue * nextValue;
-      case '÷':
-        return prevValue * nextValue;
-      case '%':
-        return (nextValue / 100) * prevValue;
-      case '**':
-        return prevValue ** nextValue;
-      case '!':
-        return getFactorial(prevValue);
-      case 'E':
-        return prevValue * (10 ** nextValue);
-      case 'log':
-        return Math.log10(nextValue);
-      case 'ln':
-        return Math.log(nextValue);
-      case '√':
-        return Math.sqrt(nextValue);
-      case 'sin':
-      case 'arcsin':
-      case 'cos':
-      case 'arccos':
-      case 'tan':
-      case 'arctan':
-        const currentFn = getTrigFn(value);
-        return currentFn(getAngle(nextValue));
-      case Math.E:
-      case Math.PI:
-        return prevValue * value;
-    }
-  });
+  const outputInRPN = buildPolishNotation(state.outputExpression);
+  const result = calcInPolishNotation(outputInRPN);
 
   state.resultValue = result;
+  state.outputExpression = [result];
 };
 
 export default equalsHandler;
 
-// польская запись?
 // проблемы с числами с плавающей точкой
