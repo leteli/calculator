ee5fc6e0c829cd6591986d98054fbbff2177f298
Nastia Kartseva <danetzachem@gmail.com>
2022-09-14 19:03:19 +0300

    Edit clear behavior on backspace

--- src/handlers/clearHandler.js
+++ src/handlers/clearHandler.js
@@ -1,10 +1,13 @@
 const clearHandler = ({ target }, state) => {
   const buttonValue = target.innerHTML;
   if (buttonValue === 'AC') {
-    state.outputExpression = [];
+    state.outputExpression = [0];
     state.resultValue = null;
   } else {
     state.outputExpression.pop();
+    if (state.outputExpression.length === 0) {
+      state.outputExpression = [0];
+    }
   }
 };
 
